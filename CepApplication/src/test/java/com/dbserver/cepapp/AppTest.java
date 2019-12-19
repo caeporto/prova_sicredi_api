83b9b6cb86054f1a06805f1d3dddb20747dbff58
caeporto <caetano.silva@acad.pucrs.br>
2019-12-19 13:12:51 -0300

    Added Scenario 1: Consult Valid CEP, also added a util package for web requests and handling JSON

--- CepApplication/src/test/java/com/dbserver/cepapp/AppTest.java
+++ CepApplication/src/test/java/com/dbserver/cepapp/AppTest.java
@@ -1,15 +1,47 @@
 package com.dbserver.cepapp;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 import org.junit.jupiter.api.Test;
+
+import org.json.JSONObject;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import com.dbserver.cepapp.util.JsonReader;
 
 public class AppTest 
 {
 	
+	private boolean assertJson(JSONObject obj, JSONObject expected)
+	{
+		return obj.similar(expected);
+	}
+	
     @Test
-    public void shouldAnswerWithTrue()
+    public void consultaCepValido()
     {
-        assertTrue( true );
-    }
+        JSONObject cep_obj = null;
+		try{
+			cep_obj = JsonReader.readJsonFromUrl("https://viacep.com.br/ws/91060900/json/");
+		} catch(IOException e) {
+		    e.printStackTrace();
+		} catch(JSONException e) {
+		    e.printStackTrace();
+		}
+		
+        JSONObject expected = new JSONObject();
+		expected.put("cep", "91060-900");
+		expected.put("logradouro", "Avenida Assis Brasil 3940");
+		expected.put("complemento", "");
+		expected.put("bairro", "São Sebastião");
+		expected.put("localidade", "Porto Alegre");
+		expected.put("uf", "R");
+		expected.put("unidade", "");
+		expected.put("ibge", "4314902");
+		expected.put("gia", "");
+		boolean compare = assertJson(cep_obj, expected);
+		assertTrue(compare);
+	}
 }
